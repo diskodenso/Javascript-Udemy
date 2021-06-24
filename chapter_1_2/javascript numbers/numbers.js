6ee071aa4cc0e69f99eef57b0c2bef3cc3429b32
Denis Olf <denisadnanolf@gmail.com>
2021-06-24 09:25:58 +0200

    commit

---
+++ chapter_1_2/javascript numbers/numbers.js
@@ -0,0 +1,19 @@
+// create a variable which has the age 28 and year 1993
+
+let age = 28;
+let year = 1993;
+
+// display in console
+console.log(age, year);
+
+//// let lets you define age again
+age = 30;
+console.log(age);
+
+// const = constant can not be overritten afterwards
+const points = 30;
+console.log(points);
+
+
+var score = 75;
+console.log(score);
